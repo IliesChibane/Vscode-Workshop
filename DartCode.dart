b482c17594b51fc48981f920bd5dfd1aa4c18d27
ilies <ilies.chibane.2000@gmail.com>
2021-11-27 17:26:16 +0100

    commit1

---
+++ DartCode.dart
@@ -0,0 +1,14 @@
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
